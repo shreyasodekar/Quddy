6b90a77367f8649c4ee1a035706989f3ff38bbd0
shreyasodekar <139267101+shreyasodekar@users.noreply.github.com>
2025-05-07 01:29:59 -0400

    Update connect_instruments.py

--- Scripts/connect_instruments.py
+++ Scripts/connect_instruments.py
@@ -2,6 +2,7 @@ import os
 import time
 from datetime import datetime
 import json
+import logging
 
 import numpy as np
 from scipy.optimize import curve_fit
@@ -15,15 +16,16 @@ from resonator import background, see, shunt
 
 import qcodes as qc
 from qcodes.dataset import Measurement
+from qcodes.logger.logger import start_all_logging
+from qcodes_contrib_drivers.drivers.OxfordInstruments.Proteox import oiDECS
 from qcodes.instrument_drivers.Keysight import KeysightP9374A
-# from qcodes.instrument_drivers.Keysight.Keysight_N5183B import KeysightN5183B
-
 from qcodes.instrument_drivers.Keysight import KeysightN5183B
 
 from qcodes.instrument_drivers.Minicircuits import MiniCircuitsRCSPDT
 from qcodes_contrib_drivers.drivers.QuTech.IVVI import IVVI
 from qick.pyro import make_proxy
 
+Proteox = oiDECS('Proteox')
 pna = KeysightP9374A('pna','TCPIP0::localhost::hislip0::INSTR')
 mxg = KeysightN5183B('mxg','TCPIP0::192.168.1.140::inst0::INSTR')
 ivvi = IVVI('ivvi', 'ASRL4::INSTR', numdacs=16, dac_step=10, dac_delay=0.01, safe_version=True, polarity=['BIP', 'BIP', 'BIP', 'BIP'], use_locks=False)
@@ -32,7 +34,7 @@ switch = MiniCircuitsRCSPDT('switch', '192.168.1.141')
 
 soc, soccfg = make_proxy(ns_host="192.168.1.138", ns_port=8888, proxy_name="rfsoc")
 
-directory = r'C:\Users\frolovlab\Documents\Python Scripts\Data\'
+directory = 'C:/Users/frolovlab/Documents/Python Scripts/Data/'
 expt = '2025_02_18_TransmonFridge_SnInAs_2x7_ParampV1.1-2_Cooldown1'
 os.chdir(directory + expt)
 print('All instruments connected')
@@ -40,7 +42,7 @@ print('Working Directory: '+ directory + expt)
 
 import win32com.client as win32
 word = win32.Dispatch('Word.Application')
-doc = word.Documents.Open(directory + expt + '\' + expt + '.docx')
+doc = word.Documents.Open(directory + expt + '/' + expt + '.docx')
 word.Selection.GoTo(What=win32.constants.wdGoToLine, Which=win32.constants.wdGoToLast)
 word.Visible = True
 selection = word.Selection
