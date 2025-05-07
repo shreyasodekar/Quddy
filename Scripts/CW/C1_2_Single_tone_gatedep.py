08423b86a35a01d759fdd585ba99f21dbe6535de
shreyasodekar <139267101+shreyasodekar@users.noreply.github.com>
2025-05-07 11:37:09 -0400

    Update C1_2_Single_tone_gatedep.py

--- Scripts/CW/C1_2_Single_tone_gatedep.py
+++ Scripts/CW/C1_2_Single_tone_gatedep.py
@@ -34,6 +34,7 @@ meas = Measurement()
 meas.register_parameter(pna.polar)
 
 data = generate_empty_nan_array(len(y_pts), len(x_pts))
+snapshot = generate_empty_nan_array(len(y_pts))
 
 # Save data.
 f = h5py.File(path+'/'+filename, 'a', libver='latest')
@@ -41,6 +42,7 @@ f.create_dataset('Metadata', data = json.dumps(config, indent=4))
 f.create_dataset('Frequency', data = x_pts)
 f.create_dataset('Power', data = y_pts)
 f.create_dataset('S21', data = data)
+f.create_dataset('Fridge snapshot', data = snapshot)
 f.swmr_mode = True
 
 
@@ -51,7 +53,9 @@ for y in tqdm(range(len(y_pts))):
     data[y] = temp
     f['S21'][:] = data
     pna.output(0)
-
+    snapshot[y] = get_fridge_snapshot()
+    f['Fridge snapshot'] = snapshot
+    
 pna.sweep_mode("CONT")
 
 
