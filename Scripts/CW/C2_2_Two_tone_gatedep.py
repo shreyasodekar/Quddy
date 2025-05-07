a644a019b041a09444f98ff3a3c8c75f83299f1b
shreyasodekar <139267101+shreyasodekar@users.noreply.github.com>
2025-05-07 11:47:36 -0400

    Update C2_2_Two_tone_gatedep.py

--- Scripts/CW/C2_2_Two_tone_gatedep.py
+++ Scripts/CW/C2_2_Two_tone_gatedep.py
@@ -28,6 +28,7 @@ pna.power(config['pna']['power'])
 mxg.power(config['mxg']['power'])
 
 data = generate_empty_nan_array(len(y_pts), len(x_pts))
+snapshot = generate_empty_nan_array(len(y_pts), len(x_pts))
 
 # # Save data.
 f = h5py.File(path+'/'+filename, 'a', libver='latest')
@@ -35,6 +36,7 @@ f.create_dataset('Metadata', data = json.dumps(config, indent = 4))
 f.create_dataset('Frequency', data = x_pts)
 f.create_dataset('Power', data = y_pts)
 f.create_dataset('S21', data = data)
+f.create_dataset('Fridge snapshot', data = snapshot)
 f.swmr_mode = True
 
 for y in tqdm(range(len(y_pts))):
@@ -76,6 +78,8 @@ for y in tqdm(range(len(y_pts))):
         data[y,x] = np.mean(temp) 
         f['S21'][:] = data
         time.sleep(0.5)
+        snapshot[y,x] = get_fridge_snapshot()
+        f['Fridge snapshot'] = snapshot
 
 pna.sweep_mode("CONT")
 
