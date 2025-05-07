2c00700b136eff894b6713892861c41aebed7ab8
shreyasodekar <139267101+shreyasodekar@users.noreply.github.com>
2025-05-07 12:01:23 -0400

    Update R4_1_2dRabi.py

--- Scripts/RFSOC/R4_1_2dRabi.py
+++ Scripts/RFSOC/R4_1_2dRabi.py
@@ -20,6 +20,7 @@ expt_cfg = {'t_start': 4,
 x_pts = np.arange(expt_cfg['g_start'],expt_cfg['g_stop'],expt_cfg['g_step'])
 y_pts = np.linspace(expt_cfg['t_start'],expt_cfg['t_stop'],expt_cfg['t_points'], dtype = 'float64')
 data = generate_empty_nan_array(len(y_pts),len(x_pts))
+snapshot = generate_empty_nan_array(len(y_pts),len(x_pts))
 
 # Save data.
 f = h5py.File(path+'/'+filename, 'a', libver='latest')
@@ -27,6 +28,7 @@ f.create_dataset('Metadata', data = json.dumps(config, indent = 4))
 f.create_dataset('Power', data = x_pts)
 f.create_dataset('Pulse_length', data = y_pts)
 f.create_dataset('S21', data = data)
+f.create_dataset('Fridge snapshot', data = snapshot)
 f.swmr_mode = True
     
 y_pts = np.linspace(expt_cfg['t_start'],expt_cfg['t_stop'],expt_cfg['t_points'])
@@ -44,6 +46,8 @@ for x in tqdm(range(len(x_pts))):
         avgi, avgq = prog.acquire(soc, progress=False)
         data[y,x] = avgi[0][0]+1j*avgq[0][0]
         f['S21'][:] = data
+        snapshot[y,x] = get_fridge_snapshot()
+        f['Fridge snapshot'] = snapshot
 
 # Plot results.
 fig = plt.figure(figsize=(16,6))
